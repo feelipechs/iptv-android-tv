ae94eb41c134a46576e6f54a7ab01e0d9cccd0a7
feelipechs <flpchagas@outlook.com>
2026-05-03 10:41:14 -0300

    chore: remove build dir from tracking

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_iptv_tv_IPTVApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.iptv.tv.IPTVApp"
-)
-public final class _com_iptv_tv_IPTVApp {
-}
